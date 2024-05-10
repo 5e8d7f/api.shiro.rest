688c10390af8f15f047c0913d98640feefe37c23
igna <igna@femboy.band>
2024-05-10 18:51:06 -0400

    chore: Move generateHash function to next/server

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -1,2 +1,13 @@
 export const moneyToCredits = (money: string) =>
   Math.floor(parseFloat(money) * 20);
+
+// TODO: move this into next/server
+export const generateHash = (length: number) => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
